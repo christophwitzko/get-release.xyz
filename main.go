9427a2348290fd7d6bc2d2e94fa57e2a44e1d1eb
Christoph Witzko <github@christophwitzko.com>
2017-04-11 12:50:50 +0200

    feat: add gracefull shutdown

--- main.go
+++ main.go
@@ -8,13 +8,18 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"os/signal"
 	"time"
 )
 
-var Client *release.GithubClient
+var bindAddress string = ":5000"
+var client *release.GithubClient
 
 func init() {
-	Client = release.NewClient(os.Getenv("GITHUB_TOKEN"))
+	if ba := os.Getenv("BIND_ADDRESS"); ba != "" {
+		bindAddress = ba
+	}
+	client = release.NewClient(os.Getenv("GITHUB_TOKEN"))
 }
 
 func logger(router http.Handler) http.Handler {
@@ -37,29 +42,47 @@ func doRedirect(w http.ResponseWriter, r *http.Request, url string, err error) {
 	http.Redirect(w, r, url, 302)
 }
 
-func GetLatestDownload(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+func getLatestDownload(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
 	ctx, cancel := context.WithTimeout(r.Context(), 5*time.Second)
 	defer cancel()
-	url, err := Client.GetLatestDownloadUrl(ctx, ps[0].Value, ps[1].Value, ps[2].Value, ps[3].Value)
+	url, err := client.GetLatestDownloadUrl(ctx, ps[0].Value, ps[1].Value, ps[2].Value, ps[3].Value)
 	doRedirect(w, r, url, err)
 }
 
-func GetMatchingDownload(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+func getMatchingDownload(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
 	ctx, cancel := context.WithTimeout(r.Context(), 5*time.Second)
 	defer cancel()
-	url, err := Client.GetMatchingDownloadUrl(ctx, ps[0].Value, ps[1].Value, ps[2].Value, ps[3].Value, ps[4].Value)
+	url, err := client.GetMatchingDownloadUrl(ctx, ps[0].Value, ps[1].Value, ps[2].Value, ps[3].Value, ps[4].Value)
 	doRedirect(w, r, url, err)
 }
 
-func Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+func getVersions(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	http.NotFound(w, r)
+}
+
+func index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	fmt.Fprintln(w, "https://get-release.xyz/:owner/:repo/:os/:arch{/:constraint}")
 }
 
 func main() {
 	router := httprouter.New()
-	router.GET("/", Index)
-	router.GET("/:owner/:repo/:os/:arch", GetLatestDownload)
-	router.GET("/:owner/:repo/:os/:arch/:constraint", GetMatchingDownload)
-	log.Println("starting server on port 5000...")
-	log.Fatal(http.ListenAndServe(":5000", logger(router)))
+	router.GET("/", index)
+	router.GET("/:owner/:repo", getVersions)
+	router.GET("/:owner/:repo/:os/:arch", getLatestDownload)
+	router.GET("/:owner/:repo/:os/:arch/:constraint", getMatchingDownload)
+	server := http.Server{
+		Addr:    bindAddress,
+		Handler: logger(router),
+	}
+	go func() {
+		log.Printf("starting server on port %s...", bindAddress)
+		if err := server.ListenAndServe(); err != nil {
+			log.Fatal(err)
+		}
+	}()
+	defer server.Shutdown(nil)
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt, os.Kill)
+	<-c
+	log.Println("server stopped")
 }
